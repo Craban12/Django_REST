8a96da593eb17f9cf82d2263fdd8b96f82af39a7
Craban12 <Antipov-sava@yandex.ru>
2022-05-24 19:25:57 +0300

    Homework 4

--- library/library/settings.py
+++ library/library/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'mainapp',
     'users',
     'todoapp',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.AdminRenderer',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
