bc671a840f75d1ad3e4df3f9b67440d2b75a5f0a
Craban12 <Antipov-sava@yandex.ru>
2022-06-04 09:17:01 +0300

    Homework 6

--- library/library/settings.py
+++ library/library/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'mainapp',
     'users',
@@ -133,14 +134,23 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-# REST_FRAMEWORK = {
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 #     'DEFAULT_RENDERER_CLASSES': [
 #         'rest_framework.renderers.JSONRenderer',
 #         'rest_framework.renderers.AdminRenderer',
 #     ],
+
 #     'DEFAULT_FILTER_BACKENDS': [
 #         'django_filters.rest_framework.DjangoFilterBackend'
 #     ],
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 #     'PAGE_SIZE': 100
-# }
+}
--- library/users/views.py
+++ library/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet, GenericViewSet, ReadOnlyModelViewSet
 from rest_framework.renderers import JSONRenderer, AdminRenderer
 
@@ -11,3 +12,4 @@ class UserViewSet(ReadOnlyModelViewSet):
     renderer_classes = (JSONRenderer,)
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    permission_classes = [AllowAny]
